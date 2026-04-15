0d79003bb465808e17b28726a394062abb9a4a86
Nelson-Lopez2008 <nel410200@gmail.com>
2026-04-15 08:50:20 -0600

    actualizacion

--- scrip.js
+++ scrip.js
@@ -1,11 +1,12 @@
-function Mostrarmensaje (cuida y proteje a los animales) {
-    let mensaje=document. getElementById("mensaje");
 
-    mensaje.innerHTML= "cuidado y protege a los animales";
-    mensaje. stylki.display ="black";
 
-    setTimeout( ()=>[
-        mensaje.style.apacity="1";
-        mensaje.style. transform="translate y (0)";
-    ],50);
+function Mostrarmensaje () {
+    let mensaje = document.getElementById("mensaje");
+    mensaje.innerHTML = "cuidado y protege a los animales";
+    mensaje.style.display ="block";
+
+    setTimeout( ()=>{
+        mensaje.style.opacity="1";
+        mensaje.style.transform="translateY(0)";
+},50);
 }
