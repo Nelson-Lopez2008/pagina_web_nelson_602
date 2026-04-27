7ee8c051569644daaca2bdc9334db6941c991ab0
Nelson-Lopez2008 <nel410200@gmail.com>
2026-04-27 14:36:05 -0600

    actualizacion

--- scrip.js
+++ scrip.js
@@ -1,7 +1,7 @@
 
 
-function Mostrarmensaje () {
-    let mensaje = document.getElementById("mensaje");
+function Mostrarmensaje2 () {
+    let mensaje = document.getElementById("mensaje2");
     mensaje.innerHTML = "cuidado y protege a los animales";
     mensaje.style.display ="block";
 
@@ -10,3 +10,15 @@ function Mostrarmensaje () {
         mensaje.style.transform="translateY(0)";
 },50);
 }
+
+function Mostrarmensaje1 () {
+    let mensaje = document.getElementById("mensaje1");
+    mensaje.innerHTML = "Amo a sheila";
+    mensaje.style.display ="block";
+
+    setTimeout( ()=>{
+        mensaje.style.opacity="1";
+        mensaje.style.transform="translateY(0)";
+},50);
+}
+
